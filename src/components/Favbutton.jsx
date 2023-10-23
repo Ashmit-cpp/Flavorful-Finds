ad7a704b87dcf939d7a0fe21bae7887875b96775
Ashmit Sharma <sashmit2003@gmail.com>
2023-10-23 21:46:31 +0530

    css fixed

--- src/components/Favbutton.jsx
+++ src/components/Favbutton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { AiFillHeart } from 'react-icons/ai';
 
 function Favbutton() {
   const navigate = useNavigate();
